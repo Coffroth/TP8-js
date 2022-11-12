cede5b7bfeed705b6f16de81f9afeff6fe3afd9b
Coffroth <morgan.coffroth@wsu.edu>
2022-11-12 18:57:52 +0000

    fix errors with js?

--- scripts.js
+++ scripts.js
@@ -1,14 +1,16 @@
 /* JavaScript code for memory card game - TP8-JS */
+
+
 window.onload = function() {
-  
+
   cardList = document.querySelectorAll(".card"); //collection of cards
-  
+
   cardCount = cardList.length; // how many cards are on table
-  
+
   for (c = 0; c < cardCount; c++) {
-    CardList[c].onclick = function(){
+    CardList[c].onclick = function() {
       this.classList.toggle("clicked");
     }
   }
-  
+
 }
