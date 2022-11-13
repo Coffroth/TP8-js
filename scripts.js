554c7bcd39ccd042981c4f03fe51d5dffa165857
Coffroth <morgan.coffroth@wsu.edu>
2022-11-13 20:46:59 +0000

    added clicks and pictures

--- scripts.js
+++ scripts.js
@@ -1,16 +1,14 @@
-/* JavaScript code for memory card game - TP8-JS */
 
 
 window.onload = function() {
-
-  cardList = document.querySelectorAll(".card"); //collection of cards
-
-  cardCount = cardList.length; // how many cards are on table
-
+  
+  cardList= document.querySelectorAll(".card");
+  
+  cardCount = cardList.length;
+  
   for (c = 0; c < cardCount; c++) {
-    CardList[c].onclick = function() {
+    cardList[c].onclick = function() {
       this.classList.toggle("clicked");
     }
   }
-
 }
